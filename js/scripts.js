6ab73b40d4894b4ea97a82a5fb2f53753c248466
Jason Brown <jasontbrown99@gmail.com>
2017-01-17 18:09:53 -0800

    refractored business logic for all statements, thus far

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,25 @@
+// BUSINESS LOGIC
+
+// this bit of code will be refractored below.  This shows each, individual steps in the business logic as desplayed on the leap-year logic grid in lesson.
+// var leapYear = function(year) {
+//   if (year % 100 === 0) {
+//     return false;
+//   } else if (year % 4 === 0) {
+//     return true;
+//   } else {
+//   return false;
+//   }
+// };
+
 var leapYear = function(year) {
-  if (year % 4 === 0) {
+  if ((year % 4 === 0) && (year % 100 !== 0)) {
     return true;
   } else {
-  return false;
+    return false;
   }
 };
 
-
-
+// USER UI LOGIC
 $(document).ready(function() {
   $("form#leap-year").submit(function(event) {
     event.preventDefault();
